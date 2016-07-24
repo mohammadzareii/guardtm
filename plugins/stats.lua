310a4f46fa4024d55cf3aa12f335f29b84f855f1
mohammadzareii <mamalitala.mz@gmail.com>
2016-07-24 01:15:35 +0000

    guard v1.0

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'shield' then -- Put everything you like :)
+  if matches[1]:lower() == 'guard' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /shield ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /guard ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "shield" then -- Put everything you like :)
+    if matches[2] == "guard" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (shield)",
-	"^[#!/]([Ss]hield)"
+    "^[#!/]([Ss]tats) (guard)",
+	"^[#!/]([Gg]uard)"
     }, 
   run = run
 }
